4bcf4cc44006e3ce0572da0be0218dbff0efcf40
floruo <florian.ruosch@uzh.ch>
2025-06-24 14:53:56 +0200

    First iteration of the DresClient
    Login

--- src/config.js
+++ src/config.js
@@ -4,15 +4,15 @@
 export const SPARQL_ENDPOINT = "http://localhost:8080/query/sparql";
 
 // DRES API endpoint
-export const DRES_API_ENDPOINT = "http://localhost:8080/api/v2";
+export const DRES_API_ENDPOINT = "https://vbs.videobrowsing.org";
 let DRES_USER = "MyUserName";
 let DRES_PASSWORD = "MyPassword";
 try {
-    const config = require("./config.local.json");
+    const config = (await import("./config.local.json")).default;
     DRES_USER = config.DRES_USER;
     DRES_PASSWORD = config.DRES_PASSWORD;
 } catch (e) {
-    console.warn("config.local.json not found or invalid. Using empty credentials.");
+    console.warn("config.local.json not found or invalid. Using default credentials.");
 }
 
 export { DRES_USER, DRES_PASSWORD };
@@ -33,4 +33,3 @@ export const FILTER_ORDER = [
 ];
 
 export const RESULTS_PER_ROW = 8;
-
